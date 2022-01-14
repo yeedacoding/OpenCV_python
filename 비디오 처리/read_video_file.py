7cc036974fb46fa291a269788f2aa9a4f4476b01
yeedacoding <taeheun0325@naver.com>
2022-01-14 22:24:12 +0900

    write and read video

---
+++ 비디오 처리/read_video_file.py
@@ -0,0 +1,31 @@
+# 저장된 동영상파일에서 프레임 가져온 후 행렬에 저장하여 읽기
+
+import cv2
+from Common.utils import put_string
+
+capture = cv2.VideoCapture("images/video_file.avi")
+if not capture.isOpened():
+    raise Exception("동영상 파일 개방 안됨")
+
+frame_rate = capture.get(cv2.CAP_PROP_FPS)
+delay = int(1000 / frame_rate)
+frame_cnt = 0
+
+while True:
+    ret, frame = capture.read()
+    if not ret or cv2.waitKey(delay) >= 0:
+        break
+    blue, green, red = cv2.split(frame)  # 컬러 영상의 채널(blue, green, red)을 분리
+    frame_cnt += 1
+
+    if 100 <= frame_cnt < 200:
+        cv2.add(blue, 100, blue)
+    elif 200 <= frame_cnt < 300:
+        cv2.add(green, 100, green)
+    elif 300 <= frame_cnt < 400:
+        cv2.add(red, 100, red)
+
+    frame = cv2.merge([blue, green, red])
+    put_string(frame, 'frame_cnt : ', (20, 30), frame_cnt)
+    cv2.imshow("Read Video File", frame)
+capture.release()
---
+++ 비디오 처리/write_camera_frame.py
@@ -0,0 +1,41 @@
+# 카메라 프레임 동영상 파일로 저장하기
+
+import cv2
+
+capture = cv2.VideoCapture(0)
+if capture.isOpened() == False:
+    raise Exception("카메라 연결 안됨")
+
+fps = 29.97  # 초당 프레임 수
+delay = round(1000/fps)  # 프레임 간 지연 시간
+size = (640, 360)   # 동영상 파일 해상도
+fourcc = cv2.VideoWriter_fourcc(*'DX50')  # 압축 코덱 설정
+
+# 카메라 속성 실행창에 출력
+print("width x height : ", size)
+print("VideoWriterfourcc : %s" % fourcc)
+print("delay : %2d" % delay)
+print("fps: %.2f" % fps)
+
+capture.set(cv2.CAP_PROP_ZOOM, 1)
+capture.set(cv2.CAP_PROP_FOCUS, 0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, size[0])
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, size[1])
+
+# 동영상파일 개방 및 코덱, 해상도 설정
+writer = cv2.VideoWriter("images/video_file.avi", fourcc, fps, size)
+if writer.isOpened() == False:
+    raise Exception("동영상파일 개방 안됨")
+
+while True:
+    ret, frame = capture.read()
+    if not ret:
+        break
+    if cv2.waitKey(delay) >= 0:
+        break
+
+    writer.write(frame)
+    cv2.imshow("View Frame from Camera", frame)
+
+writer.release()
+capture.release()
