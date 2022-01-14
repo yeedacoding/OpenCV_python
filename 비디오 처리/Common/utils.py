7c01ddd994ea0927ae9c3f7f9dfe9d2c1f74b682
yeedacoding <taeheun0325@naver.com>
2022-01-14 22:22:38 +0900

    module

---
+++ 비디오 처리/Common/utils.py
@@ -0,0 +1,11 @@
+import cv2
+import time
+import numpy as np
+
+
+def put_string(frame, text, pt, value, color=(120, 200, 90)):
+    text += str(value)
+    shade = (pt[0] + 2, pt[1] + 2)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, text, shade, font, 0.7, (0, 0, 0), 2)
+    cv2.putText(frame, text, pt, font, 0.7, color, 2)
