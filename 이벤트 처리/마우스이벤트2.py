4d37b73018af1b7f56c976e140cc4648359e0c08
yeedacoding <taeheun0325@naver.com>
2022-01-22 17:33:32 +0900

    Mouse Event (draw rectangle)

---
+++ 이벤트 처리/마우스이벤트2.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+drawing = False
+ix, iy = -1, -1
+
+
+def draw_rectangle(event, x, y, flags, params):
+    global ix, iy, drawing
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix, iy = x, y
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
+
+
+img = np.zeros((512, 512, 3))
+
+cv2.namedWindow('my_drawing')
+cv2.setMouseCallback('my_drawing', draw_rectangle)
+
+while True:
+    cv2.imshow('my_drawing', img)
+
+    if cv2.waitKey(0):
+        break
+
+cv2.destroyAllWindows()
