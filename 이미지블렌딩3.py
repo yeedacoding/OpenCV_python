691c0a25b6de373a8bcefcd4e2505b5eb1d1d68f
yeedacoding <taeheun0325@naver.com>
2022-04-08 16:41:28 +0900

    Image Blend (using bitwise operator)

--- 이미지블렌딩3.py
+++ 이미지블렌딩3.py
@@ -18,11 +18,11 @@ img2 = cv2.resize(img2, (300, 300))
 # img1에서 roi설정 후 img2에 합성해보기
 # img1 = (854, 1280, 3)
 print(img1.shape)
+print(img2.shape)
 
+# roi는 img1(원본) 상에서 img2(logo)만큼의 size로 지정
 x_offset = 1280 - 300
 y_offset = 854 - 300
-
-print(img2.shape)
 
 rows, cols, channels = img2.shape
 
@@ -30,32 +30,45 @@ roi = img1[y_offset:854, x_offset:1280]
 # plt.imshow(roi)
 # plt.show()
 
+
 # img2에서 빨간 부분 마스킹
+
+# logo img를 binary 이미지로 바꾸기
 img2gray = cv2.cvtColor(img2, cv2.COLOR_RGB2GRAY)
 
-plt.imshow(img2gray, cmap='gray')
+ret, mask = cv2.threshold(img2gray, 60, 255, cv2.THRESH_BINARY)
+
+plt.imshow(mask, cmap='gray')
 plt.show()
 
-mask_inv = cv2.bitwise_not(img2gray)
+# 색상 반전
+mask_inv = cv2.bitwise_not(mask)
 
+plt.title('mask inverted')
 plt.imshow(mask_inv, cmap='gray')
 plt.show()
-print(mask_inv.shape)
-white_background = np.full(img2.shape, 255, dtype=np.uint8)
 
-bk = cv2.bitwise_or(white_background, white_background, mask=mask_inv)
-print(bk.shape)
+# roi 이미지에서 mask_inv(logo부분이 검은색 = 0)만큼 0값을 부여 = roi 이미지에서 logo 모양을 뚫고 그 안에 다시 logo를 채움
+bk = cv2.bitwise_and(roi, roi, mask=mask_inv)
 
-# plt.imshow(bk)
-# plt.show()
+plt.title('bk')
+plt.imshow(bk)
+plt.show()
 
-fg = cv2.bitwise_or(img2, img2, mask=mask_inv)
+# img2(원본 logo)에 mask(logo를 제외한 부분 = 0)을 적용해 기존 logo(빨강색)을 제외한 부분에 0값을 부여하여 logo만 살리기
+fg = cv2.bitwise_and(img2, img2, mask=mask)
+
+plt.title('fg')
 plt.imshow(fg)
 plt.show()
 
-final_roi = cv2.bitwise_or(roi, fg)
-# plt.imshow(final_roi)
-# plt.show()
+# bk = roi 이미지에서 logo 모양만 0 값, fg = 기존 logo 이미지에서 logo 모양만 원본(빨강)으로 남겨둠
+# bk와 fg를 합치면 roi에 뚫려있던 logo모양이 채워짐
+final_roi = cv2.add(bk, fg)
+
+plt.title('final roi')
+plt.imshow(final_roi)
+plt.show()
 
 large_img = img1
 small_img = final_roi
