61b4e95a13665c0e4d0d4e18a9bd90e9608dc9ee
yeedacoding <taeheun0325@naver.com>
2022-04-10 16:15:43 +0900

    Mouse Event(click, draw circle)

--- 이벤트 처리/마우스이벤트.py
+++ 이벤트 처리/마우스이벤트.py
@@ -5,7 +5,7 @@ import cv2
 def onMouse(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         # 마우스 왼쪽 클릭 시 원 그리기
-        cv2.circle(image, (x, y), 100, (255, 255, 255), -1)
+        cv2.circle(image, (x, y), 100, (0, 255, 0), -1)
     elif event == cv2.EVENT_RBUTTONDOWN:
         print("마우스 오른쪽 버튼 누르기")
     elif event == cv2.EVENT_RBUTTONUP:
@@ -14,14 +14,14 @@ def onMouse(event, x, y, flags, param):
         print("마우스 왼쪽 버튼 더블클릭")
 
 
-cv2.namedWindow('Mouse Event')
+cv2.namedWindow(winname='Mouse Event')
 cv2.setMouseCallback('Mouse Event', onMouse)
 
 
-image = np.zeros((512, 512, 3), np.uint8)
+image = np.zeros((512, 512, 3))
 
 while True:
     cv2.imshow('Mouse Event', image)
-    if cv2.waitKey(0):
+    if cv2.waitKey(20) & 0xFF == 27:
         break
 cv2.destroyAllWindows()
